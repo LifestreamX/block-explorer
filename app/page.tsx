b64e43d72ff7539cd4636f2b44733124149468a0
haloandfable <tylerallen@live.com>
2023-12-03 19:04:04 -0500

    feat: Implement responsive and styled footer UI. Designed and implemented a responsive footer with a clean and modern look. Ensured a consistent and visually appealing style across various screen sizes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Hero from './components/Hero';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
-        sapiente?
-      </h1>
+      <Hero />
     </main>
   );
 }
